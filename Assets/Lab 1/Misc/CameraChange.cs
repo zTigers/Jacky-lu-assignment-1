9f9aec2916df3ebaf4ca2add3d1493a1368d20b5
zTigers <36008743+zTigers@users.noreply.github.com>
2018-10-03 13:36:56 -0400

    Assignment 1

---
+++ Assets/Lab 1/Misc/CameraChange.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+public class CameraChange : MonoBehaviour
+{
+    public Camera[] cameras = new Camera[3];
+    public bool changeAudioListener = true;
+    void Update()
+    {
+        if (Input.GetKeyDown("0"))
+        {
+            EnableCamera(cameras[0], true);
+            EnableCamera(cameras[1], false);
+            EnableCamera(cameras[2], false);
+        }
+        if (Input.GetKeyDown("1"))
+        {
+            EnableCamera(cameras[0], false);
+            EnableCamera(cameras[1], true);
+            EnableCamera(cameras[2], false);
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            EnableCamera(cameras[0], false);
+            EnableCamera(cameras[1], false);
+            EnableCamera(cameras[2], true);
+        }
+    }
+    private void EnableCamera(Camera cam, bool
+    enabledStatus)
+    {
+        cam.enabled = enabledStatus;
+        if (changeAudioListener)
+            cam.GetComponent<AudioListener>().enabled =
+            enabledStatus;
+    }
+}
---
+++ Assets/Lab 1/Misc/SecurityChange.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+public class SecurityChange : MonoBehaviour
+{
+    public Camera[] cameras = new Camera[3];
+    public bool changeAudioListener = true;
+    void Update()
+    {
+        if (Input.GetKeyDown("3"))
+        {
+            EnableCamera(cameras[0], true);
+            EnableCamera(cameras[1], false);
+            EnableCamera(cameras[2], false);
+        }
+        if (Input.GetKeyDown("4"))
+        {
+            EnableCamera(cameras[0], false);
+            EnableCamera(cameras[1], true);
+            EnableCamera(cameras[2], false);
+        }
+        if (Input.GetKeyDown("5"))
+        {
+            EnableCamera(cameras[0], false);
+            EnableCamera(cameras[1], false);
+            EnableCamera(cameras[2], true);
+        }
+        if (Input.GetKeyDown("m"))
+        {
+            EnableCamera(cameras[0], false);
+            EnableCamera(cameras[1], false);
+            EnableCamera(cameras[2], false);
+        }
+    }
+    private void EnableCamera(Camera cam, bool
+    enabledStatus)
+    {
+        cam.enabled = enabledStatus;
+        if (changeAudioListener)
+            cam.GetComponent<AudioListener>().enabled =
+            enabledStatus;
+    }
+}
